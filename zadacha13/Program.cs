7165fc4d87c60698a2e15323a1823995a8c7a18a
lera <lera_bal@list.ru>
2022-12-17 15:13:55 +0300

    zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number / 100 > 0) 
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является цифра {number.ToString()[2]}");
+}
+else 
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры");
+}
+
+Console.WriteLine();
+
