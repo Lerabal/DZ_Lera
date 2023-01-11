864ed24a6a8da737a53c9f2ca53fbe0183cb441f
lera <lera_bal@list.ru>
2023-01-11 12:54:28 +0300

    zadacha27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма цифр = {sum}");
