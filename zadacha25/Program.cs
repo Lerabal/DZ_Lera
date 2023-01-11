6ba6755550f4b6637f3735e8d6325caaccca7c5e
lera <lera_bal@list.ru>
2023-01-11 13:22:15 +0300

    zadacha25_

--- zadacha25/Program.cs
+++ zadacha25/Program.cs
@@ -3,17 +3,20 @@
 //3, 5 -> 243 (3⁵)
 
 Console.Clear();
-Console.Write ("Введите число1: ");
-int num1 = int.Parse(Console.ReadLine()!);
-Console.Write ("Введите число2: ");
-int num2 = int.Parse(Console.ReadLine()!);
-int count = 1;
-int result = num1;
-
-while (count < num2)
+void Stepen()
 {
-    result = result * num1;
-    count++;
+    Console.Write("Введите число1: ");
+    int num1 = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите число2: ");
+    int num2 = int.Parse(Console.ReadLine()!);
+    int count = 1;
+    int result = num1;
+    while (count < num2)
+    {
+        result = result * num1;
+        count++;
+    }
+    Console.WriteLine($"{num1} в {num2} степени = {result}");
 }
+Stepen();
 
-Console.WriteLine($"{num1} в {num2} степени = {result}");
