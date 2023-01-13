d9a767867e596d2bb81da075c84359e29db5e97f
lera <lera_bal@list.ru>
2023-01-13 16:25:26 +0300

    zadacha28

---
+++ zadacha28/Program.cs
@@ -0,0 +1,25 @@
+﻿// 28. Написать программу вычисления произведения чисел от 1 до N
+//4 - -> 24
+//5 --> 120
+
+Console.Clear();
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int getMultiplic(int start, int end)
+{
+    int multip = 1;
+    for (int i = 1; i <= end; i++)
+    {
+        multip = multip * i;
+    }
+    return multip;
+}
+
+int num = getUserData("Введите число N");
+int result = getMultiplic(1, num);
+Console.Write(result);
