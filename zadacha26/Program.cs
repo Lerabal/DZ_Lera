f9821c73f27a3373ab6554e33cc87a0d624c2e39
lera <lera_bal@list.ru>
2023-01-13 16:17:57 +0300

    zadacha26

---
+++ zadacha26/Program.cs
@@ -0,0 +1,34 @@
+﻿// 26. Определить количество цифр в числе
+
+Console.Clear();
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getNumberOfDigit(int number)
+{
+    int numberOfDigit = 0;
+    while (number > 0)
+    {
+        number = number/10;
+        numberOfDigit++;
+    }
+    return numberOfDigit;
+}
+
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int number = getUserData("Введите число");
+int numberOfDigit = getNumberOfDigit(number);
+showData($"В числе {number} цифр = ", numberOfDigit);
