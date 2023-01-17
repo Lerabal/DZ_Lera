880015152d7d278efc127d92f50cd9e2dbb42b21
lera <lera_bal@list.ru>
2023-01-17 14:25:46 +0300

    zadacha32

---
+++ zadacha32/Program.cs
@@ -0,0 +1,49 @@
+﻿// 32. Написать программу замену элементов массива на противоположные
+
+
+int[] generateArray(int lenght, int start, int end)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+
+int getIntFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine()!);
+    return userInt;
+}
+
+int[] invertArray(int[] arrayToInvert)
+{
+    for (int i = 0; i < arrayToInvert.Length; i++)
+    {
+        arrayToInvert[i] = -arrayToInvert[i];
+    }
+    return arrayToInvert;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+int lenght = getIntFromUser("Введите длину массива");
+int[] array = generateArray(lenght, -100 ,100);
+printArray(array);
+int[] invertedArray = invertArray(array);
+printArray(invertedArray);
+
