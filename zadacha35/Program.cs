f82cbcc5a49fa7ef0e6ea8f6f1b3a1d5b8f35b2a
lera <lera_bal@list.ru>
2023-01-17 15:32:17 +0300

    zadacha35

---
+++ zadacha35/Program.cs
@@ -0,0 +1,57 @@
+﻿//  35. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+
+int[] FillArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int Find(int[] array, int start, int end)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= start && array[i] <= end)
+        {
+            count += 1;
+        }
+
+    }
+    return count;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+
+int number = getUserData("Введите количество элементов в массиве");
+int[] array = FillArray(number, 1, 20);
+printArray(array);
+Console.WriteLine();
+int start = getUserData("Введите начальное число диапазона");
+int end = getUserData("Введите конечное число диапазона");
+
+int count = Find(array, start, end);
+Console.WriteLine($"Количество элементов в диапазоне [{start}, {end}] = {count}");
