3d7de8b75fd5080db269b59ef4f64862554648c0
lera <lera_bal@list.ru>
2023-01-20 08:51:55 +0300

    zadacha40

---
+++ zadacha40/Program.cs
@@ -0,0 +1,32 @@
+﻿// 40.Принимает на вход три числа и проверяет может ли существовать треугольник с сторонами такой длины. Теорема  о неравенстве треугольника:
+// каждая сторона треуг меньше суммы двух других сторон
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+bool IsTriangleExist(int a, int b, int c)
+{
+    bool isExist = false;
+    if (a < b + c && b < c + a && c < b + a)
+    {
+        isExist = true;
+    }
+    return isExist;
+}
+
+int a = getUserData("Введите длину стороны А");
+int b = getUserData("Введите длину стороны B");
+int c = getUserData("Введите длину стороны C");
+bool isExist = IsTriangleExist(a, b, c);
+
+Console.WriteLine($"Треугольник со сторонами [{a}, {b}, {c}] {((isExist)?"существует":"не существует")}");
+
+/*
+string str;
+if (isExist)
+{str = "Существует";}
+else {str = "неСуществует";}*/
