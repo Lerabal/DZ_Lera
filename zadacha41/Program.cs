e8c0cc2c8c77fbd9bf133e47b3fde3eb7c7b52cc
lera <lera_bal@list.ru>
2023-01-20 14:10:58 +0300

    zadacha43

---
+++ zadacha41/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int size = 5;               
+int count = 0;
+
+while (size > 0)
+{
+    int num1 = getUserData("Введите число");
+    
+   
+    if (num1 > 0) count++;
+    size--;
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
---
+++ zadacha43/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+double b1 = getUserData("Введите b1: ");
+double k1 = getUserData("Введите k1: ");
+double b2 = getUserData("Введите b2: ");
+double k2 = getUserData("Введите k2: ");
+double cord = Koord(b1,k1,b2,k2);
+double cord1 = Koord1(b2,k2);
+
+double Koord(double b1, double k1, double b2, double k2)
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    return x; 
+    }
+
+double Koord1(double b2, double k2)
+{
+    double y = k2 * cord + b2;
+    return y;
+}
+ 
+Console.WriteLine($"Прямые пересекутся в точке с координатами X: {cord}, Y: {cord1}");
+
