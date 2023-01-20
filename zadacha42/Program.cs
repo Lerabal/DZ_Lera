0bac656bb52440db1c0c5474db98304f2d5c70c1
lera <lera_bal@list.ru>
2023-01-20 09:52:09 +0300

    zadacha42

---
+++ zadacha42/Program.cs
@@ -0,0 +1,22 @@
+﻿// 42. Преобразовать число в двоичное 
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string TransformBinary(int number)
+{
+    string result = string.Empty;
+    while (number > 0)
+    {
+        result = number % 2+result;
+        number = number / 2;
+    }
+    return result;
+}
+int number = getUserData("Введите число: ");
+string binary = TransformBinary(number);
+Console.WriteLine ($"{number} = {binary}");
