5700eb72e0a896e0ab92a09c2a9d8bf7d132d387
lera <lera_bal@list.ru>
2023-01-24 09:48:11 +0300

    zadacha49

---
+++ zadacha49/Program.cs
@@ -0,0 +1,101 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+int GetDataFromUser(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int[,] AddArray(int colLength, int rowLength, int start, int finish)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+    }
+    return array;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printArray(int[,] array)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + " \t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + " \t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            //Console.Write(array[i, j] + " \t");
+            if (i == j)
+            {
+                printInColor(array[i, j] + " \t");
+            }
+            else
+            {
+                Console.Write(array[i, j] + " \t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindSum(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + array[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+void Change(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = (int)Math.Pow(array[i, j], 2);
+            }
+        }
+    }
+    return;
+}
+
+
+
+int colLength = GetDataFromUser("Введите количество строк ");
+int rowLength = GetDataFromUser("Введите количество столбцов");
+int[,] array = AddArray(colLength, rowLength, 0, 10);
+printArray(array);
+Console.WriteLine();
+int sum = FindSum(array);
+Console.WriteLine($"Сумма элементов главной диагонали = {sum}");
