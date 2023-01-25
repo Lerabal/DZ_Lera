bff67b6749ee4d0a7e5bda5e8b58d5913d2365fa
lera <lera_bal@list.ru>
2023-01-25 13:29:17 +0300

    save

--- zadacha58/Program.cs
+++ zadacha58/Program.cs
@@ -7,3 +7,54 @@
 18 20
 15 18
 */
+int[,] AddArray(int colLength, int rowLength, int start, int finish)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish);
+        }
+    }
+    return array;
+}
+
+int[,] MultiplyArray(int[,] array1, int[,] array2)
+{
+    int[,] result = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            result [i, j] = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+            result[i, j] += array1[i, k] * array2[k,j];
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array1 = AddArray(2, 2, 1, 10);
+int[,] array2 = AddArray(2, 2, 1, 10);
+int[,] result = MultiplyArray(array1, array2);
+Console.WriteLine("Первая матрица:");
+PrintArray(array1);
+Console.WriteLine("Вторая матрица:");
+PrintArray(array2);
+Console.WriteLine("Произведение матриц:");
+PrintArray(result);
