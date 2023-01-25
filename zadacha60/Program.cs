e70e3e1b8ba34ce0a3671187ee4acf699f95d5ea
lera <lera_bal@list.ru>
2023-01-25 14:22:20 +0300

    add3dmassiv

--- zadacha60/Program.cs
+++ zadacha60/Program.cs
@@ -5,3 +5,40 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+int[,,] AddArray(int yLength, int colLength, int rowLength, int start, int finish)
+{
+    int[,,] array = new int[yLength, colLength, rowLength];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(start, finish);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine("Страница №: " + (i + 1));
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]} ({i},{j},{k}) ");
+            }
+             Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array1 = AddArray(2, 2, 2, 10, 100);
+Console.WriteLine();
+PrintArray(array1);
+
