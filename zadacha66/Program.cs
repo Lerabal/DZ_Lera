e15b482115834f1b2948379f2eee34d68afcd311
lera <lera_bal@list.ru>
2023-01-27 10:02:06 +0300

    zadacha66

---
+++ zadacha66/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetDataFromUser(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int Sum(int start, int end)
+{
+    if (start > end) return end + Sum(end + 1, start);
+    if (start == end) return end;
+    else return start + Sum(start + 1, end);
+
+}
+
+int m = GetDataFromUser("Задайте значение от M: ");
+int n = GetDataFromUser("Задайте значение до N: ");
+int sum = Sum(m,n);
+Console.WriteLine($"Cуммa натуральных элементов в промежутке от {m} до {n} = {sum}");
+
+
